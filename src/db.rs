e0af19c1c3be9f1c43183ae341c1bf6ef7a4d45a
k-wachira <abkelvinwachira@gmail.com>
2026-03-06 13:20:32 +0300

    Initial commit with insert and fetch

---
+++ src/db.rs
@@ -0,0 +1,78 @@
+use crate::models::{DbLog, EntryLog};
+
+use rusqlite::{Connection, Result};
+use std::process;
+
+pub struct Database {
+    conn: Connection,
+}
+
+impl Database {
+    //Initialize a new DB
+    pub fn new(path: &str) -> Result<Self, String> {
+        let conn = Connection::open(path).unwrap();
+        let db = Database { conn: conn };
+
+        db.initialize()?;
+
+        Ok(db)
+    }
+
+    //Create table if it does not exist
+    fn initialize(&self) -> Result<(), String> {
+        self.conn
+            .execute_batch(
+                "
+            CREATE TABLE IF NOT EXISTS shadow_logs (
+            id    INTEGER PRIMARY KEY,
+            content  TEXT NOT NULL,
+            energy INTEGER,
+            mood INTEGER,
+            weather TEXT,
+            location TEXT,
+            time_stamp TEXT,
+            device TEXT NOT NULL,
+            log_type TEXT
+        )",
+            )
+            .unwrap_or_else(|_| {
+                process::exit(1);
+            });
+        Ok(())
+    }
+
+    //Inserts a single log
+    pub fn insert(&self, log: &EntryLog) -> Result<(), rusqlite::Error> {
+        self.conn.execute(
+            "INSERT INTO shadow_logs (content, energy, mood, weather, location, time_stamp, device, log_type)
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
+            (&log.content, &log.energy, &log.mood, &log.weather, &log.location, &log.time_stamp, &log.device, &log.log_type),
+        )?;
+
+        Ok(())
+    }
+
+    //Fetches all logs
+    pub fn get_logs(&self, limit: Option<usize>) -> Result<Vec<DbLog>, rusqlite::Error> {
+        let query = match limit {
+            Some(n) => format!(
+                "SELECT id, content, energy, mood, weather FROM shadow_logs LIMIT {}", n ),
+            None => "SELECT id, content, energy, mood, weather FROM shadow_logs".to_string(),
+        };
+
+        let mut stmt = self.conn.prepare(&query)?;
+
+        let logs: Vec<DbLog> = stmt
+            .query_map([], |row| {
+                Ok(DbLog {
+                    id: row.get(0)?,
+                    content: row.get(1)?,
+                    energy: row.get(2)?,
+                    mood: row.get(3)?,
+                    weather: row.get(4)?,
+                })
+            })?
+            .collect::<Result<Vec<_>, _>>()?;
+        return Ok(logs);
+    }
+}
