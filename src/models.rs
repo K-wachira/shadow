dfa7aa4e0fd29cd4ccaadc7ec5ca1e70694a8ccb
k-wachira <abkelvinwachira@gmail.com>
2026-03-06 22:22:48 +0300

    Align the EntryLog Struct with table structure

--- src/models.rs
+++ src/models.rs
@@ -13,10 +13,18 @@ pub struct RawLog {
 }
 
 #[derive(Debug)]
-pub struct DbLog {
+pub struct EntryLog {
     pub id: i32,
     pub content: String,
-    pub energy: i32, 
-    pub mood: i32,
-    pub weather: String,
+    pub energy: Option<i32>, 
+    pub mood: Option<i32>,
+    pub weather: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct FileIngest {
+    pub id: Option<i32>,
+    pub file_name: String,
+    pub time_stamp: String,
+    pub is_ingested: Option<bool
 }
