4ba220def1aaa056dc6c0cbb7b412d2bbc75e322
k-wachira <abkelvinwachira@gmail.com>
2026-03-29 12:47:30 +0300

    refactor(tui): refactor truncate

---
+++ shadow-tui/src/tui/utils.rs
@@ -0,0 +1,8 @@
+// ─── Utility ─────────────────────────────────────────────────────────────────
+pub fn truncate(s: &str, max: usize) -> String {
+    if s.chars().count() <= max {
+        s.to_string()
+    } else {
+        format!("{}…", s.chars().take(max).collect::<String>())
+    }
+}
