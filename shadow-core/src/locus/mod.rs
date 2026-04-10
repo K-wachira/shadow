c2fe90a2ad6315fd1b974cee00f28ce59182de84
k-wachira <abkelvinwachira@gmail.com>
2026-04-10 14:18:52 +0300

    refactor(locus): rename core to lucas

--- shadow-core/src/locus/mod.rs
+++ shadow-core/src/locus/mod.rs
@@ -1,7 +1,7 @@
-pub mod core;
+pub mod locus;
 pub mod mind_op;
 
-pub use core::Locus;
+pub use locus::Locus;
 pub use mind_op::extract_affected_fields;
 pub use mind_op::process_ingested_logs;
 pub use mind_op::reflect;
