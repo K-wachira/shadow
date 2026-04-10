cce3a5b6e98d5a39b63cc44c0019a08bbef5b6ee
k-wachira <abkelvinwachira@gmail.com>
2026-04-10 13:50:23 +0300

    perf(services): improve error handling

--- shadow-services/src/ingest.rs
+++ shadow-services/src/ingest.rs
@@ -3,14 +3,14 @@ use crate::models::RawLog;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn process_json_file(log_name: &String, dir: &PathBuf) -> Result<RawLog, String> {
+pub fn process_json_file(log_name: &String, dir: &PathBuf) -> color_eyre::Result<RawLog> {
     let complete_path =
         dirs::home_dir()
             .unwrap()
             .join(format!("{}{}", &dir.to_string_lossy(), &log_name));
 
-    let content = std::fs::read_to_string(complete_path).map_err(|e| e.to_string())?;
-    let raw: RawLog = serde_json::from_str(&content).map_err(|e| e.to_string())?;
+    let content = std::fs::read_to_string(complete_path).map_err(|e| color_eyre::eyre::eyre!(e));
+    let raw: RawLog = serde_json::from_str(&content?).map_err(|e| color_eyre::eyre::eyre!(e))?;
     Ok(raw)
 }
 
@@ -42,9 +42,9 @@ pub fn process_json_file(log_name: &String, dir: &PathBuf) -> Result<RawLog, Str
 //     Ok(ingested)
 // }
 
-pub fn get_files(dir: &PathBuf) -> Result<Vec<String>, String> {
+pub fn get_files(dir: &PathBuf) -> color_eyre::Result<Vec<String>> {
     let files: Vec<String> = fs::read_dir(&dir)
-        .map_err(|e| e.to_string())?
+        .map_err(|e| color_eyre::eyre::eyre!(e))?
         .filter_map(|e| e.ok())
         .map(|e| e.file_name().to_string_lossy().to_string())
         .collect();
